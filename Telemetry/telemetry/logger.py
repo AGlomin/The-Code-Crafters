b66bd550a20142b0a4d41b5c4285f577f69693bf
AGlomin <arg218@exeter.ac.uk>
2026-02-08 09:03:27 +0000

    correct API

--- Telemetry/telemetry/logger.py
+++ Telemetry/telemetry/logger.py
@@ -1,26 +1,22 @@
-# Ran directly from source
 try:
     from events import TelemetryEvent
     from validator import validate_event, detect_anomalies, TelemetryValidationError
     from storage import write_event
 except:
-    # Ran from mini_test
-    try:
-        from telemetry.events import TelemetryEvent
-        from telemetry.validator import validate_event, detect_anomalies, TelemetryValidationError
-        from telemetry.storage import write_event
-    # Ran from game
-    except:
-        from Telemetry.telemetry.events import TelemetryEvent
-        from Telemetry.telemetry.validator import validate_event, detect_anomalies, TelemetryValidationError
-        from Telemetry.telemetry.storage import write_event
+    from telemetry.events import TelemetryEvent
+    from telemetry.validator import validate_event, detect_anomalies, TelemetryValidationError
+    from telemetry.storage import write_event
 
-def log_event(event_type: str, stage_id: int, payload: dict):
+
+def log_event(event_type, stage_id, session_id, user_id, payload):
+
     try:
         event = TelemetryEvent.create(
             event_type=event_type,
             stage_id=stage_id,
-            payload=payload
+            session_id=session_id,
+            user_id=user_id,
+            payload=payload,
         )
 
         validate_event(event)
@@ -31,5 +27,4 @@ def log_event(event_type: str, stage_id: int, payload: dict):
         print(f"[Telemetry Validation Error] {e}")
 
     except Exception as e:
-        # Telemetry must NEVER crash the game
         print(f"[Telemetry Error] {e}")
