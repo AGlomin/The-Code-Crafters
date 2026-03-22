20affa4ae6df70fdf986512660e79ffed85f2203
autisnt <cw1168@exeter.ac.uk>
2026-03-22 16:13:17 +0000

    Add enemy 1 sprite sheet, including slight change in colour for visibility

--- classes.py
+++ classes.py
@@ -188,7 +188,7 @@ class AGENT:
         xRender, yRender, sizeRender, yDir = self.getRenderPosAndSize(tiles, moveFrame, True)
         animFrame = frame % 4
         # Medic has animation (supports any with name eventually)
-        if self.spritesheetName in ["medic", "brawler", "bomber", "en0"]:
+        if self.spritesheetName in ["medic", "brawler", "bomber", "en0", "en1"]:
             spritesheetCol = animFrame
             """
             ROWS:
